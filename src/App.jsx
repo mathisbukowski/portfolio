84fbabc72f98f55a958242a73900d7c141cf0cab
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-05-29 21:36:25 +0200

    [-] removing navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import StarField from "./Components/StarField.jsx";
-import Navbar from "./Components/Navbar.jsx";
 import Hero from "./Components/Hero.jsx";
 import Works from "./Components/Works.jsx";
 import Footer from "./Components/Footer.jsx";
@@ -8,7 +7,6 @@ function App() {
   return (
       <>
           <StarField />
-          <Navbar />
           <Hero />
           <Works />
           <Footer />
