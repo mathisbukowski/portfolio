ac188b25f443b34bb7d122aba21828427a59040d
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-05-30 20:38:58 +0200

    [~] editing the works section and fix the bug with the memoji who walks in the works section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,46 +3,52 @@ import { motion } from "framer-motion";
 const MotionDiv = motion.div;
 
 export default function Hero() {
-    return (
-        <div className="text-white">
-            <div
-                className="max-w-[calc(100vw-40px)] mt-[-96px] w-full h-screen mx-auto px-6 flex md:flex-row flex-col items-center">
-                <MotionDiv
-                    initial={{ x: -1000 }}
-                    animate={{ x: 0 }}
-                    transition={{ duration: 0.5, type: "spring", stiffness: 120 }}
-                    className="md:w-1/2 w-full px-4 md:px-8 mt-36 flex justify-center items-center"
-                >
-                    <div className="flex flex-col justify-center items-center w-full max-w-md text-center bg-nightblue p-10 rounded-lg shadow-lg mx-auto">
-                        <h1 className="text-3xl font-bold py-4">
-                            Hi there ! 👋
-                        </h1>
-                        <p className="text-2xl">
-                            I&apos;m Mathis, a technology enthusiast and developer. <br />
-                            I&apos;m currently living in Lille 🇫🇷 and studying at Epitech&ensp;
-                            <span><img src="/logo-epi.png" alt="Epitech" className="inline-block w-5 h-5 align-middle ml-1" /></span>&ensp;!
-                        </p>
-                        <button
-                            className="bg-skyblue w-[200px] rounded-md font-extrabold my-6 py-3 text-textColor"
-                        >
-                            Continue !
-                        </button>
-                    </div>
-                </MotionDiv>
 
-                <MotionDiv
-                    initial={{ x: 1000 }}
-                    animate={{ x: 0 }}
-                    transition={{ duration: 0.5, type: "spring", stiffness: 120 }}
-                    className="md:w-1/2 w-full px-4 md:px-8 mt-36 flex justify-center items-center"
-                >
-                    <img
-                        src={"/hello.png"}
-                        className="w-[370px] h-auto rounded-3xl transition-opacity duration-1000 ease-in-out -mr-10"
-                        alt="Mathis"
-                    />
-                </MotionDiv>
-            </div>
+    const scrollToSection = (id) => {
+        document.getElementById(id).scrollIntoView({
+            behavior: "smooth"
+        });
+    };
+
+    return (
+        <div className="text-white flex flex-col md:flex-row items-center justify-center min-h-screen">
+            <MotionDiv
+                initial={{x: -1000}}
+                animate={{x: 0}}
+                transition={{duration: 0.5, type: "spring", stiffness: 120}}
+                className="md:w-1/2 w-full px-4 md:px-8 flex justify-center items-center mt-8 md:mt-0"
+            >
+                <div
+                    className="flex flex-col justify-center items-center w-full max-w-md text-center bg-nightblue p-10 rounded-lg shadow-lg mx-auto">
+                    <h1 className="text-3xl font-bold py-4">
+                        Hi there ! 👋
+                    </h1>
+                    <p className="text-2xl">
+                        I&apos;m Mathis, a technology enthusiast and developer. <br/>
+                        I&apos;m currently living in Lille 🇫🇷 and studying at Epitech&ensp;
+                        <span><img src="/logo-epi.png" alt="Epitech"
+                                   className="inline-block w-5 h-5 align-middle ml-1"/></span>&ensp;!
+                    </p>
+                    <button
+                        onClick={() => scrollToSection("works")}
+                        className="bg-skyblue w-[200px] rounded-md font-extrabold my-6 py-3 text-textColor"
+                    >
+                        Continue !
+                    </button>
+                </div>
+            </MotionDiv>
+            <MotionDiv
+                initial={{x: 1000}}
+                animate={{x: 0}}
+                transition={{duration: 0.5, type: "spring", stiffness: 120}}
+                className="md:w-1/2 w-full px-4 md:px-8 flex justify-center items-center"
+            >
+                <img
+                    src={"/hello.png"}
+                    className="w-[370px] h-auto rounded-3xl transition-opacity duration-1000 ease-in-out"
+                    alt="Mathis"
+                />
+            </MotionDiv>
         </div>
     );
 }
--- src/Components/Works.jsx
+++ src/Components/Works.jsx
@@ -1,58 +1,8 @@
 import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
 
 const MotionDiv = motion.div;
 
-const projects = [
-    {
-        title: "Railess",
-        description: "An App to streamline your journeys.",
-        technologies: ["React", "Node.js", "JavaScript", "TailwindCSS","MySQL", "Docker", "Git"],
-        link: "https://railess.fr",
-        logo: "/logorailess.png",
-        color: "bg-skyblue",
-    },
-    {
-        title: "Sam",
-        description: "An App to see wildfire around the world. App made in 2 days",
-        technologies: ["React", "Node.js", "MySQL", "Git", "JavaScript", "TailwindCSS"],
-        link: "https://github.com/mathisbukowski/jam-03",
-        logo: "/logosam.png",
-        color: "bg-corail",
-    },
-    {
-        title: "Corewar",
-        description: "The goal of this, is to reproduice a fight between 2 to 4 champions in a memory.",
-        technologies: ["C", "Git"],
-        link: "https://github.com/mathisbukowski/corewar",
-        logo: "/corewar.jpeg",
-        color: "bg-anthracite"
-    },
-    {
-        title: "Morpheus",
-        description: "This project is a creation of VMs (Ubuntu Server & Arch/Debian DualBoot)",
-        technologies: ["Arch Linux", "Debian", "Ubuntu", "Bash"],
-        link: "https://github.com/mathisbukowski/",
-        logo: "/archlinux-logo.svg",
-        color: "bg-sky-gray"
-    },
-    {
-        title: "MyRadar",
-        description: "This project has been made to reproduice an air traffic control radar",
-        technologies: ["C", "CSFML", "Git"],
-        link: "https://github.com/mathisbukowski/",
-        logo: "/myradar.jpg",
-        color: "bg-mint"
-    },
-    {
-        title: "EpyTODO",
-        description: "This project has been made for discover the backend.",
-        technologies: ["JavaScript", "Node.js", "Git", "MySQL"],
-        link: "https://github.com/mathisbukowski/",
-        logo: "/backend.jpeg",
-        color: "bg-gold"
-    }
-];
+import projects from "../projects.json";
 
 const Icon = ({ name }) => {
     switch (name) {
@@ -91,7 +41,7 @@ const Icon = ({ name }) => {
 
 export default function Works() {
     return (
-        <div className="text-white mt-40">
+        <div className="text-white mt-40" id="works">
             <header className="bg-blue-night p-6 text-center">
                 <h1 className="text-4xl font-bold">My Works</h1>
                 <p className="text-xl mt-2">Here are some of my projects and achievements</p>
